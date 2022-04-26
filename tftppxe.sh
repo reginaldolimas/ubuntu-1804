0980fa46d86de6790e09029fcfd014574c393f3a
reginaldolimas <79094085+reginaldolimas@users.noreply.github.com>
2022-04-26 11:44:08 -0300

    Update tftppxe.sh

--- tftppxe.sh
+++ tftppxe.sh
@@ -71,25 +71,7 @@ if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
 		exit 1
 fi
 #
-# Verificando se as dependências do Tftpd-Hpa Server e PXE/Syslinux estão instaladas
-# opção do dpkg: -s (status), opção do echo: -e (interpretador de escapes de barra invertida), -n (permite nova linha)
-# || (operador lógico OU), 2> (redirecionar de saída de erro STDERR), && = operador lógico AND, { } = agrupa comandos em blocos
-# [ ] = testa uma expressão, retornando 0 ou 1, -ne = é diferente (NotEqual)
-echo -n "Verificando as dependências do Tftpd-Hpa Server, PXE/Syslinux e NFS Server, aguarde... "
-	for name in nfs-kernel-server
-	do
-		[[ $(dpkg -s $name 2> /dev/null) ]] || { 
-			echo -en "\n\nO software: $name precisa ser instalado. \nUse o comando 'apt install $name'\n";
-			deps=1; 
-			}
-	done
-		[[ $deps -ne 1 ]] && echo "Dependências.: OK" || { 
-			echo -en "\nInstale as dependências acima e execute novamente este script\n";
-			echo -en "Recomendo utilizar o script: dnsdhcp.sh para resolver as dependências."
-			exit 1; 
-			}
-		sleep 5
-#
+
 # Script de instalação do Tftpd-Hpa Server e PXE/Syslinux no GNU/Linux Ubuntu Server 18.04.x
 # opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
 # opção do comando hostname: -I (all IP address)
